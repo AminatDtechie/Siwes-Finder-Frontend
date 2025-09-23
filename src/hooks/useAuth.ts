01efa16ce1c5bb1e7fb9ba846258d8bf4543948b
Megabyte-webdev <afolabimubarak18@gmail.com>
2025-09-23 05:02:07 +0100

    text restructured

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,93 +2,94 @@ import { axiosClient } from "@/services/axios-client";
 import { onSuccess } from "@/utils/notifications/OnSuccess";
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
-import { onFailure } from '../utils/notifications/OnFailure';
+import { onFailure } from "../utils/notifications/OnFailure";
 import { useAuthorize } from "@/context/AuthContext";
+import { extractErrorMessage } from "@/utils/formmaters";
 
-const useAuth =()=>{
-  const navigate= useNavigate();
+const useAuth = () => {
+  const navigate = useNavigate();
   const { saveLogin } = useAuthorize();
 
- const client= axiosClient(null);
+  const client = axiosClient(null);
 
- const loginMutation = useMutation({
-  mutationFn: async(credentials)=>{
-    const { data }= await client.post("/login", credentials)
-    if (!data.token){
-      throw new Error("Invalid response: User data not found");
-    }
-    return data;
-  },
-  onSuccess: (data)=>{
-    saveLogin( data.token , data.role )
-    onSuccess({
-      message: "Login Successful!",
-      success: "Continuing to dashboard"
-    });
-    navigate("/dashboard");
-  },
-  onError: ( error ) => {
-    console.log( "error" , error)
-    onFailure({
-      message: error.message,
-      error: "Something went wrong"
-    })
-  }
- })
+  const loginMutation = useMutation({
+    mutationFn: async (credentials) => {
+      const { data } = await client.post("/login", credentials);
+      if (!data.token) {
+        throw new Error("Invalid response: User data not found");
+      }
+      return data;
+    },
+    onSuccess: (data) => {
+      saveLogin(data.token, data.role);
+      onSuccess({
+        message: "Login Successful!",
+        success: "Continuing to dashboard",
+      });
+      navigate("/dashboard");
+    },
+    onError: (error) => {
+      console.log("error", error);
+      onFailure({
+        message: "Failed to Login!",
+        error: extractErrorMessage(error) || "Something went wrong",
+      });
+    },
+  });
 
- const studentRegisterMutation = useMutation({
-  mutationFn: async(credentials)=>{
-    const { data }= await client.post("/students/register", credentials)
-    if (!data?.data){
-      throw new Error("Something went wrong: Please try again");
-    }
-    return data;
-  },
-  onSuccess: ()=>{
-    onSuccess({
-      message: "Account created successfully!",
-      success: "Please check your mail to verify your account and proceed to login"
-    });
-    navigate("/login");
-  },
-  onError: ( error ) => {
-    onFailure({
-      message: error.message,
-      error: "Something went wrong"
-    })
-  }
- })
+  const studentRegisterMutation = useMutation({
+    mutationFn: async (credentials) => {
+      const { data } = await client.post("/students/register", credentials);
+      if (!data?.data) {
+        throw new Error("Something went wrong: Please try again");
+      }
+      return data;
+    },
+    onSuccess: () => {
+      onSuccess({
+        message: "Account created successfully!",
+        success:
+          "Please check your mail to verify your account and proceed to login",
+      });
+      navigate("/login");
+    },
+    onError: (error) => {
+      onFailure({
+        message: "Failed to register student",
+        error: extractErrorMessage(error) || "Something went wrong",
+      });
+    },
+  });
 
+  const recruiterRegisterMutation = useMutation({
+    mutationFn: async (credentials) => {
+      const { data } = await client.post("/recruiters/register", credentials);
+      if (!data?.data) {
+        throw new Error("Something went wrong: Please try again");
+      }
+      return data;
+    },
+    onSuccess: () => {
+      onSuccess({
+        message: "Account created successfully!",
+        success:
+          "Please check your mail to verify your account and proceed to login",
+      });
+      navigate("/login");
+    },
+    onError: (error) => {
+      onFailure({
+        message: "Failed to register recruiter",
+        error: extractErrorMessage(error),
+      });
+    },
+  });
 
- const recruiterRegisterMutation = useMutation({
-  mutationFn: async(credentials)=>{
-    const { data }= await client.post("/recruiters/register", credentials)
-    if (!data?.data){
-      throw new Error("Something went wrong: Please try again");
-    }
-    return data;
-  },
-  onSuccess: ()=>{
-    onSuccess({
-      message: "Account created successfully!",
-      success: "Please check your mail to verify your account and proceed to login"
-    });
-    navigate("/login");
-  },
-  onError: ( error ) => {
-    onFailure({
-      message: error.message,
-      error: "Something went wrong"
-    })
-  }
- })
-
-return {
-  login:loginMutation,
-  studentRegister: studentRegisterMutation,
-  recruiterRegister: recruiterRegisterMutation
-}
-
-}
+  return {
+    login: loginMutation,
+    studentRegister: studentRegisterMutation,
+    recruiterRegister: recruiterRegisterMutation,
+  };
+};
 
 export default useAuth;
